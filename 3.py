a7e1dbe6a705bb6f3034e356a3561849bda1624a
vaithschmitz <vaithschmitz@googlemail.com>
2020-06-24 22:23:14 +0100

    3

---
+++ 3.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def is_prime(i):
+    for x in range(2, i):
+        # if 2 or 3
+        if i == 2 or i == 3: 
+            return True
+        # if divisible by 2 
+        elif i % 2 == 0 or i < 2: 
+            return False
+        elif x % 2 != 0:
+            # if not divisible by 2 and i divisible by x
+            if i % x == 0:
+                return False
+
+
+def largest_prime_factor(n):
+    factors = []
+    for num in range(2, n):
+        if n % num == 0:
+            # if n is prime add to list
+            if is_prime(num) == None or is_prime(num) == True:
+                factors.append(num)
+            # check if list of primes multiplies up to n
+            if np.prod(factors) == n: 
+                factors.sort()
+                print(factors[-1])
+                return
+
+
+
+largest_prime_factor()
