a538c4b6a512cb3d2a215f63a6afdc3f4e0590a0
vaithschmitz <vaithschmitz@googlemail.com>
2020-06-25 23:08:12 +0100

    4

--- 4.py
+++ 4.py
@@ -1,29 +1,21 @@
 def largest_palindrome_product(n):
     res = 0
-    out = 0
-    j = 1
+    out = []
     i = n
-    while n >= i:
-        res  = n * i
-        print(i)
-        print(n)
-        # if palindrome, write to list
-        if list(map(int, str(res))) == list(map(int, str(res)))[::-1]:
-            out = res 
-            
-        if n == i:
-            while n >= j:
-                res  = n * j  
-                if list(map(int, str(res))) == list(map(int, str(res)))[::-1]:
-                    out = res 
-                    j -=1
-                else: 
-                    j-=1
-        n-=1
 
-    print(out)
-
-    # dec in reverse, kill after first check
+    # outer dec loop through n
+    while i >= 1:
+        # full run of inner decrementing loop (n, 1) for each outer n
+        while n >= 1 and i != 1:
+            # multiple outer n and inner n
+            res = i*n
+            # if palindrome add to list
+            if list(map(int, str(res))) == list(map(int, str(res)))[::-1]:
+                out.append(res)
+            n -=1
+        n = 999
+        i-=1
+    print(sorted(out)[-1])
 
 
 
