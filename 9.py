19a89a6301c178461b99eacdf53c635fed3a5ad4
vaithschmitz <vaithschmitz@googlemail.com>
2020-11-15 12:59:12 +0000

    9 timer

--- 9.py
+++ 9.py
@@ -1,11 +1,16 @@
 # min val for c == 335 to confirm a<b<c == 1000 
 # max val for c == 997
+import time
 a=1
 b=2
 c=3
+start = time.time()
 def is_triplet(a,b,c):
     if a**2 + b**2 == c**2:
         print(a*b*c)
+        end = time.time()
+        duration = end -start
+        print(f'time: {duration}')
 
 for c in range(335, 997):
     for b in range(2, c-b):
